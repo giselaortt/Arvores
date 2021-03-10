6c9eb16873cc16bcb0a0e0b6da56322179b605da
igor-taconi <igortaconi1@gmail.com>
2021-03-09 22:30:14 -0300

    [UPDATE] privatiza funções de uso exclusivo

--- AVL/avl.py
+++ AVL/avl.py
@@ -17,9 +17,9 @@ class Arvore:
         if(self.raiz is None):
             self.raiz = novo
         else:
-            self.inserir_recursao(self.raiz, novo)
+            self._inserir_recursao(self.raiz, novo)
 
-    def inserir_recursao(self, node, novo_no):
+    def _inserir_recursao(self, node, novo_no):
         if node.id == novo_no.id:
             print("não foi possivel inserir o novo nó, pois não são aceitas repetições.")
             return
@@ -28,29 +28,29 @@ class Arvore:
                 node.dir = novo_no
                 novo_no.pai = node
             else:
-                self.inserir_recursao(node.dir, novo_no)
+                self._inserir_recursao(node.dir, novo_no)
         else:
             if(node.esq is None):
                 node.esq = novo_no
                 novo_no.pai = node
             else:
-                self.inserir_recursao(node.esq, novo_no)
+                self._inserir_recursao(node.esq, novo_no)
 
     def buscar(self, id):
-        node = self.busca(self.raiz, id)
+        node = self._tras_ai(self.raiz, id)
         return node.nome
 
-    def tras_ai(self, node, id):
+    def _tras_ai(self, node, id):
         if node is None:
             return False
         if node.id == id:
             return node
         if id > node.id:
-            return self.busca(node.dir, id)
-        return self.busca(node.esq, id)
+            return self._tras_ai(node.dir, id)
+        return self._tras_ai(node.esq, id)
 
     def remover(self, id):
-        node = self.tras_ai(self.raiz, id)
+        node = self._tras_ai(self.raiz, id)
         if node is False:
             print("nó não encontrado na arvore. tente novamente.")
             return
@@ -117,5 +117,5 @@ class Arvore:
         pass
 
 
-    def altura_recursao(node):
+    def _altura_recursao(self, node):
         pass
