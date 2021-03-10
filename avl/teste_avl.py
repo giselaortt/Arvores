9a762b519c2bb0dfc82b83fbb2853026ad83c192
giselaortt <gisela.ortt@usp.br>
2021-03-10 03:56:49 -0300

    ADD teste para a avl

--- avl/teste_avl.py
+++ avl/teste_avl.py
@@ -1,1 +1,20 @@
 from avl import AVL
+
+
+arv = AVL()
+
+# inserindo usuários
+arv.inserir(3, 'Igor')
+arv.inserir(6, 'Jurema')
+arv.inserir(1, 'Lidia')
+arv.inserir(4, 'Carlos')
+arv.inserir(24, 'gi')
+arv.inserir(2, 'aurora')
+
+# buscando usuários
+teste_busca1 = arv.buscar(3)
+print(teste_busca1)
+teste_busca2 = arv.buscar(4)
+print(teste_busca2)
+teste_busca3 = arv.buscar(9)
+print(teste_busca3)
