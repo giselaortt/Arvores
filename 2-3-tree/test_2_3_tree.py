3fdf51209980dcec66cfeeb68bea8da7c3b2cb6d
gisela ortt <ms.giselaortt@gmail.com>
2022-11-04 20:38:32 -0300

    removal of variable numberOfKeys, simplified the insert key method and remove child, added split method in order to simlyfy the tree insertion.

--- 2-3-tree/test_2_3_tree.py
+++ 2-3-tree/test_2_3_tree.py
@@ -106,8 +106,8 @@ class Test_node():
         tree.insert( 0 )
         tree.insert( 2 )
         tree.insert( 3 )
-        """tree.insert( 4 )
         tree.insert( 5 )
+        """tree.insert( 4 )
         tree.insert( 6 )
         tree.insert( 7 )
         tree.insert( 8 )"""
--- 2-3-tree/tree_2_3.py
+++ 2-3-tree/tree_2_3.py
@@ -12,21 +12,13 @@ class Node():
         #should it be a deque instead ?
         self.keys = [ key ]
         self.numberOfChildren = 0
-        self.numberOfKeys = 1
         self.parent = parentNode
 
 
     def isLeaf( self ) -> bool:
 
-        return ( self.children[0] is None and self.children[1] is None and self.children[2] is None )
-
-
-    def __copy__( self ):
-        pass
-
-
-    def __deepcopy__( self ):
-        pass
+        #return ( self.children[0] is None and self.children[1] is None and self.children[2] is None )
+        return (self.numberOfChildren==0)
 
 
     @dispatch( int )
@@ -96,12 +88,12 @@ class Node():
 
     def isTwoNode( self ) -> bool:
 
-        return self.numberOfKeys==1
+        return len(self.keys)==1
 
 
     def isThreeNode( self ) -> bool:
 
-        return self.numberOfKeys == 2
+        return len(self.keys) == 2
 
 
     def insertChild( self, child:object ) -> bool:
@@ -109,26 +101,19 @@ class Node():
         self.numberOfChildren += 1
         if( key < self.keys[0] ):
             self.children[0] = child
-        elif( self.numberOfKeys == 1 or key < self.keys[1] ):
+        elif( self.isTwoNode() or key < self.keys[1] ):
             self.children[1]=child
         else:
             self.children[2]=child
 
 
-    def insertKey( self, newKey:int ) -> None:
-        #if( self.numberOfKeys == 2 ):
-        #    raise Exception( "This operation is not permitted" )
-        self.numberOfKeys += 1
-        if( newKey > self.keys[0] ):
-            self.keys.append(newKey)
-        else:
-            self.keys.insert(0, newKey)
-            del self.children[2]
-            self.children.insert( 0, None )
+    def insertKey( self, key:int ) -> None:
+        self.keys.append(key)
+        self.keys.sort()
 
 
     def getSecondtKey( self ) -> int:
-        if( this.numberOfKeys == 1 ):
+        if( len(self.keys) == 1 ):
             raise Exception( "This operation is not permitted" )
         return self.keys[1]
 
@@ -137,15 +122,10 @@ class Node():
         return self.keys[0]
 
 
-    def removeChild( self, key:int )->None:
-        if(self.children[2] is not None and key in self.children[2]):
-            self.children[2] = None
-
-        elif(self.children[0] is not None and key in self.children[0]):
-            self.children[0] = None
-
-        elif(self.children[1] is not None and key in self.children[1]):
-            self.children[1] = None
+    def removeChild( self, child:object ) -> None:
+        for i in range( len(self.children) ):
+            if( self.children[i] == child ):
+                self.children[i] = None
 
 
     def isFistChild():
@@ -156,7 +136,20 @@ class Node():
         pass
 
 
-    def isThirdChild():
+    def isThirdChild( self ):
+        pass
+
+
+    def split( self ):
+        newLeftNode = Node( self.keys[0] )
+        newRightNode = Node( self.keys[2] )
+        newLeftNode.children = self.children[0:2]
+        newRightNode.children = self.children[2:4]
+        self.children = [newLeftNode, newRightNode]
+        self.keys = [self.keys[1]]
+
+
+    def insertNode( self ):
         pass
 
 
@@ -199,7 +192,7 @@ class Tree_2_3():
         if( key < node.keys[0] ):
             return _search( key, node.children[0] )
 
-        if( node.isTreeNode() and key > node.keys[1] ):
+        if( node.isThreeNode() and key > node.keys[1] ):
             return _search( key, node.children[2] )
 
         return _search( key, node.children[1] )
@@ -215,31 +208,20 @@ class Tree_2_3():
             node.insertKey( key )
             return
 
-        keys = node.keys
-        keys.append( key )
-        keys.sort()
-        newLeftNode = Node(keys[0], parentNode = node)
-        newRightNode = Node(keys[-1], parentNode = node)
-        node.keys = [ keys[1] ]
-        #node.children[0] = newLeftNode
-        #node.children[1] = newRightNode
+        node.insertKey(key)
+        print("k = ", node.keys)
+        node.split()
 
-        if( node.parent == None ):
-            return
-
-        if( node.parent.isTwoNode() ):
-           return
-
-        #missing general case and needs a refactor. the function is too big.
+        if(node.parent is not None):
+            parent = node.parent
+            if( node.parent is not None ):
+                node.parent.removeChild( node )
+            node.parent = None
+            parent.insertNode( node )
 
 
     def _insertOnThreeNode( node, newNode ):
-        keys = [node.keys[2], node.keys[0], newNode.keys[2] ]
-        children = []
-        keys.sort
-        node.parent.removeChild( node.key )
-        newLeftNode     = Node( keys[0],  )
-        newRightNode    = Node( keys[2],  )
+        node.parent.removeChild( node )
 
 
     @staticmethod
@@ -254,12 +236,12 @@ class Tree_2_3():
             return node
 
         if( key < node.keys[0] ):
-            return _findNodeToInsert( key, node.children[0] )
+            return Tree_2_3._findNodeToInsert( key, node.children[0] )
 
-        if( node.isTreeNode() and key > node.keys[1] ):
-            return _findNodeToInsert( key, node.children[2] )
+        if( node.isThreeNode() and key > node.keys[1] ):
+            return Tree_2_3._findNodeToInsert( key, node.children[2] )
 
-        return _findNodeToInsert( key, node.children[1] )
+        return Tree_2_3._findNodeToInsert( key, node.children[1] )
 
 
     def remove( self, key ):
