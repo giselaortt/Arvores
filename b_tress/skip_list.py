6ebd2984f44cfbf7d9c09ba1e70bd32838dde6b4
gisela ortt <ms.giselaortt@gmail.com>
2022-12-07 21:52:15 -0300

    coded search and iterators, started insertion but did not have time to finish it today.

--- b_tress/skip_list.py
+++ b_tress/skip_list.py
@@ -1,3 +1,9 @@
+import random
+from typing import Type
+
+
+INFINITY = float('inf')
+NEGATIVE_INFINITY = float('-inf')
 
 
 class Node:
@@ -22,6 +28,10 @@ class Node:
         return self.key > key
 
 
+    def __del__( self ):
+        pass
+
+
     def __eq__( self, key:int ):
 
         return self.key==key
@@ -29,13 +39,14 @@ class Node:
 
 
 class SkipList:
-    upper_left:'Node' = None
-    upper_right:'Node' = None
+    upper_left:'Node' = Node(NEGATIVE_INFINITY)
+    upper_right:'Node' = Node(INFINITY)
     number_of_levels:int = 0
     length:int = 0
 
     def __init__( self ):
-        pass
+        self.upper_left.right = self.upper_right
+        self.upper_left.left = self.upper_left
 
 
     def __len__( self ):
@@ -43,8 +54,19 @@ class SkipList:
         return self.length
 
 
+    def __next__( self, node:Type[Node] )-> Type[Node]:
+        while( True ):
+            if( node.right is not None ):
+                return node.right
+            if( node.bellow is not None ):
+                node = node.bellow
+            else:
+                return node
+
+
     def __iter__( self ):
-        pass
+
+        return self.upper_left
 
 
     def __contains__( self, key ):
@@ -55,7 +77,7 @@ class SkipList:
         pass
 
 
-    def __add__( self, other:'SkipList' ):
+    def __add__( self, other:'SkipList' ) -> 'SkipList':
         pass
 
 
@@ -63,24 +85,59 @@ class SkipList:
         pass
 
 
-    @classmethod
-    def _flip_coin() -> bool:
+    def __del__( self ):
         pass
+
+
+    @classmethod
+    def _flip_coin(cls) -> bool:
+
+        return random.randint(0,1)
 
 
     def search( self, key ):
+        if( self.length == 0 ):
+            return None
+        node = self._search(key)
+        if(node.key == key):
+            return node
+        return None
+
+
+    def getnextnode( self, node:Type[Node], key:int ) -> Type[Node]:
         pass
 
 
-    def _search( self, key ):
+    def _search( self, key:int ):
+        node = self.upper_left
+        while( node.key != key ):
+            while( node.right is not None and node.right.key <= key ):
+                node = node.right
+            if(node.bellow is not None):
+                node = node.bellow
+            else:
+                break
+
+        return node
+
+
+    def add_level( self, node:Type[Node] )->None:
         pass
 
 
-    def insert( self, key ):
-        pass
+    def insert( self, key:int )->None:
+        node = self._search(key)
+        other_node = node.right
+        inserted = Node(key)
+        node.right = inserted
+        inserted.left = node
+        other_node.left = inserted
+        inserted.right = other_node
+        self.length += 1
 
 
     def remove( self, key ):
         pass
+
 
 
--- b_tress/test_skip_list.py
+++ b_tress/test_skip_list.py
@@ -4,7 +4,6 @@ from skip_list import Node, SkipList
 
 class Test:
 
-
     def test_node_comparison(self):
         node = Node(50)
         assert (node == 50)
@@ -17,6 +16,37 @@ class Test:
     def test_are_all_pointers_none(self):
         node = Node(50)
         assert (node.above is None and node.bellow is None and node.right is None and node.left is None)
+
+
+    def test_coin_flip(self):
+        value = SkipList._flip_coin()
+        assert (value==1 or value==0)is True
+
+
+    def test_search_on_empty_list_should_return_none(self):
+        skip = SkipList()
+        assert skip.search(50) is None
+
+
+    def test_insert_one_element_and_finds_it(self):
+        skip = SkipList()
+        assert skip.upper_left is not None
+        skip.insert(1)
+        node =  skip._search(1)
+        assert node.key == 1
+
+
+    def test_are_all_elements_sorted(self):
+        pass
+
+
+    def test_iter( self ):
+        pass
+
+
+    def test_next( self ):
+        pass
+
 
 
 
